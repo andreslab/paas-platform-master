45033dd278e1416de655212e2022b192c42abf9f
jaime andrade <jaimeandrade454@gmail.com>
2020-01-30 20:26:37 -0500

    simple page of list companies

---
+++ lib/pages/company.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class CompanyPage extends StatefulWidget {
+  @override
+  _CompanyPageState createState() => _CompanyPageState();
+}
+
+class _CompanyPageState extends State<CompanyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "Search",
+                contentPadding: EdgeInsets.symmetric(vertical: 10)),
+          ),
+          Expanded(child: gridView)
+        ],
+      ),
+    );
+  }
+
+  var gridView = new GridView.builder(
+        itemCount: 20,
+        gridDelegate:
+            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return new GestureDetector(
+            child: new Card(
+              elevation: 5.0,
+              child: new Container(
+                alignment: Alignment.center,
+                child: new Text('Item $index'),
+              ),
+            ),
+            onTap: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                child: new CupertinoAlertDialog(
+                  title: new Column(
+                    children: <Widget>[
+                      new Text("GridView"),
+                      new Icon(
+                        Icons.favorite,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  content: new Text("Selected Item $index"),
+                  actions: <Widget>[
+                    new FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: new Text("OK"))
+                  ],
+                ),
+              );
+            },
+          );
+        });
+}
